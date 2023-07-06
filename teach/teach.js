650a90f4acc28e78e9cfee2b38a8c36daf9f7339
buzzlightyear90 <beecave00@gmail.com>
2023-07-06 08:19:41 -0500

    adding the table into the teachers guide page and fixing any css/js errors

---
+++ teach/teach.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function(){
+  // Accordion Action
+  const accordionItem = document.querySelectorAll(".accordion-item");
+
+  accordionItem.forEach((el) =>
+    el.addEventListener("click", () => {
+      if (el.classList.contains("active")) {
+        el.classList.remove("active");
+      } else {
+        accordionItem.forEach((el2) => el2.classList.remove("active"));
+        el.classList.add("active");
+      }
+    })
+  );
+});
